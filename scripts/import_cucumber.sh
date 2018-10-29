a786d448eebe7c855f0f27cbd7eac33c5d24ead7
dgerike <dg@doozer.de>
2018-10-29 14:45:07 +0100

    add script to fetch all cucumber tests from JIRA (xray)

---
+++ scripts/import_cucumber.sh
@@ -0,0 +1,3 @@
+#!/bin/bash      
+
+curl -X GET 'https://xray.cloud.xpand-it.com/api/v1/export/cucumber?keys=XT-27' --header "Authorization: Bearer $XRAY_BEARER_TOKEN" | busybox unzip - -d ../features
