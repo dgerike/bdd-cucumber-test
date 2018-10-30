7d3812c562f1713d16a7482b1cc601c1117eff39
dgerike <dg@doozer.de>
2018-10-30 16:50:50 +0100

    always override existing gherkin files when importing from xray

--- scripts/import_cucumber.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash      
-
-curl -X GET 'https://xray.cloud.xpand-it.com/api/v1/export/cucumber?keys=XT-27' --header "Authorization: Bearer $XRAY_BEARER_TOKEN" | busybox unzip - -d ../features
