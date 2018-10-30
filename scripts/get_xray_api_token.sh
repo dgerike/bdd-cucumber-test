6173f93069da44a6a482ebb175a0a787771a72de
dgerike <dg@doozer.de>
2018-10-30 17:01:53 +0100

    renaming credentials file

--- scripts/get_xray_api_token.sh
+++ scripts/get_xray_api_token.sh
@@ -1,5 +1,5 @@
 #!/bin/bash      
 
-TOKEN=`curl -X POST 'https://xray.cloud.xpand-it.com/api/v1/authenticate' -H "Content-Type: application/json" --data @"credentials.json"`
+TOKEN=`curl -X POST 'https://xray.cloud.xpand-it.com/api/v1/authenticate' -H "Content-Type: application/json" --data @"xray_credentials.json"`
 
 echo ${TOKEN:1:-1}
