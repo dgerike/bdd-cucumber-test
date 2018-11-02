6dcaea15a658cef8c9867daf272279dcd2d662bd
dgerike <dg@doozer.de>
2018-11-02 18:11:01 +0100

    make upload script more flexible

--- scripts/import_cucumber_tests_from_xray.sh
+++ scripts/import_cucumber_tests_from_xray.sh
@@ -1,3 +1,3 @@
 #!/bin/bash      
 
-curl -X GET 'https://xray.cloud.xpand-it.com/api/v1/export/cucumber?keys=XT-27' --header "Authorization: Bearer $XRAY_BEARER_TOKEN" | busybox unzip -o - -d ../features
+curl -X GET "https://xray.cloud.xpand-it.com/api/v1/export/cucumber?keys=$JIRA_TEST_PLAN_ID" -H "Content-Type: application/json" -H "Authorization: Bearer $XRAY_BEARER_TOKEN" | busybox unzip -o - -d ../features
